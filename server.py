feb8df720d4c05de2d9e2cb9db89566a7bbe4eb2
pathak Mohit <81065818+pathakmohit@users.noreply.github.com>
2022-04-02 11:37:00 +0530

    this project is use socket programming in python
    this project is work like this small sample is how to connect the two to more device and how to communicate  device and 
    
    work project 
    first run server.py in python editor
    later run client in editor
    
    one secrets of the project few change to this project work like  chat 
    
    excecated    same process first run  server and client

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+
+s = socket.socket()
+print ('socket created')
+s.bind(('localhost',9999))
+
+s.listen(3)
+print('wait the connections')
+
+while True:
+    c, addr = s.accept()
+    name = c.recv(1024).decode()
+    print("connected with", addr,name)
+    c.send(bytes('WELCOME  to the SERVICE','utf-8'))
+    c.close()
